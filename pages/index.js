1d5c9941bc529bc8be8bd811d16a8ea909b90fb9
juares3453 <juarescanalle@gmail.com>
2024-07-13 17:58:51 +0000

    ajuste frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
 
-    return <h1>Amor, te amo mais que tudo na minha vida </h1>
+    return <h1>Amor, te amo mais que tudo na minha vida, mais é mais que tudo mesmoooo!!! !!</h1>
 }
 
 export default Home;
